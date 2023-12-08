bf1b0c0458f72e860a548276be21366043b55c40
nakataku0709 <soccer.tuesday.0709@ezweb.ne.jp>
2023-12-08 17:24:55 +0900

    マイグレーション設定

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Deliverables</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="antialiased">
+        <h1></h1>
+    </body>
+</html>
