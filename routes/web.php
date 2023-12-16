dce7d7983f6f3aefd5f096b0fc5122a496506d40
nakataku0709 <soccer.tuesday.0709@ezweb.ne.jp>
2023-12-16 13:51:05 +0900

    削除機能

--- routes/web.php
+++ routes/web.php
@@ -35,5 +35,6 @@ Route::get('/posts/{post}', [PostController::class, 'show'])->name('show');
 Route::post('/posts', [PostController::class, 'store']);
 Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::put('posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
 
 require __DIR__.'/auth.php';
